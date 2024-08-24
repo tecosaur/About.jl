2c0cb43d0e3b52d51b5dcc616a6cbf960d8bf3af
TEC <git@tecosaur.net>
2024-08-24 12:01:00 +0800

    Add license headers to covered files

--- src/functions.jl
+++ src/functions.jl
@@ -1,3 +1,7 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 function about(io::IO, fn::Function)
     source = Main.InteractiveUtils.which(parentmodule(fn), nameof(fn))
     methodmodules = getproperty.(methods(fn).ms, :module)
--- src/utils.jl
+++ src/utils.jl
@@ -1,3 +1,7 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 const FACE_CYCLE = [:about_cycle1, :about_cycle2, :about_cycle3, :about_cycle4]
 
 function humansize(bytes::Integer)
